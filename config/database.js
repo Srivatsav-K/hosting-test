f4cafe88183f1a530bea740648e3a12688f3355b
Srivatsav-K <srivatsav11000@gmail.com>
2022-08-10 19:50:11 +0530

    mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const configDB = () => {
-    mongoose.connect('mongodb://localhost:27017/notes-redux')
+    mongoose.connect('mongodb+srv://srivatsav:WtfPIzPi2A3WczXl@deploy-test.wxaydrj.mongodb.net/?retryWrites=true&w=majority')
         .then(() => console.log('connected to db'))
         .catch((err) => console.log('error connecting to db', err))
 }
